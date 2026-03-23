a60ec4ac87ed39d4098d0057ee1e371029346ed2
Billy Alvear <alvearbilly@gmail.com>
2026-03-22 20:27:21 -0500

    configure to support uuid and winston

--- jest.config.ts
+++ jest.config.ts
@@ -92,7 +92,9 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '^(\.{1,2}/.*)\.js$': '$1',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -178,21 +180,14 @@ const config: Config = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '^.+\.tsx?$': [
-      'ts-jest',
-      {
-        tsconfig: {
-          module: 'CommonJS',
-        },
-      },
-    ],
+    '^.+\.tsx?$': ['ts-jest', { tsconfig: { module: 'CommonJS' } }],
+    '^.+\.js$': ['ts-jest', { tsconfig: { module: 'CommonJS', allowJs: true }, diagnostics: false }],
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "\\\\node_modules\\\\",
-  //   "\\.pnp\\.[^\\\\]+$"
-  // ],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(uuid)/)',
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
